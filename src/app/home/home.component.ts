12e3ca1b627680df56c772dc2965d133349c14f6
LebzoDev <leboundiayee123@gmail.com>
2021-10-03 20:54:13 +0000

    last commit from old ubuntu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,12 +17,12 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
-    const mapProperties = {
-         center: new google.maps.LatLng(35.2271, -80.8431),
-         zoom: 15,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+    // const mapProperties = {
+    //      center: new google.maps.LatLng(35.2271, -80.8431),
+    //      zoom: 15,
+    //      mapTypeId: google.maps.MapTypeId.ROADMAP
+    // };
+    // this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
  }
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
